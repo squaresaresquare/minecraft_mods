91aafd04617fa7b611202fbb30bcf4268b42aecf
Sean-Paul Bobadilla <sean-paulbobadilla@Sean-Pauls-iMac.local>
2026-04-21 19:36:52 -0700

    first commit

---
+++ architecture-1.21.11/src/main/java/org/seanpaulbobadilla/Architecture.java
@@ -0,0 +1,16 @@
+package org.seanpaulbobadilla;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Architecture implements ModInitializer {
+	public static final String MOD_ID = "architecture";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ architecture-1.21.11/src/main/java/org/seanpaulbobadilla/mixin/ArchitectureClient.java
@@ -0,0 +1,10 @@
+package org.seanpaulbobadilla.mixin;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ArchitectureClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
