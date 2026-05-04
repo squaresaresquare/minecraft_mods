83f41150ae9b01ee47acd2601a2135a4b34b3c35
Sean-Paul Bobadilla <seanpaul.humphrey@gmail.com>
2026-05-04 15:18:52 -0700

    add changes to HalfQuartzPillar.java

--- architecture-1.21.11/src/main/java/org/seanpaulhumphrey/architecture/block/custom/HalfQuartzPillar.java
+++ architecture-1.21.11/src/main/java/org/seanpaulhumphrey/architecture/block/custom/HalfQuartzPillar.java
@@ -16,6 +16,7 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
+import net.minecraft.world.level.LevelProperties.*;
 import org.jetbrains.annotations.Nullable;
 import net.minecraft.util.math.Direction;
 import org.seanpaulhumphrey.architecture.block.entity.custom.HalfQuartzPillarEntity;
