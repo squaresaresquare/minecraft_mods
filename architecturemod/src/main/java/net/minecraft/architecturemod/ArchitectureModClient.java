21da951d6be31ac4c7f83923d5b366613351c166
Sean-Paul Bobadilla <seanpaul.humphrey@gmail.com>
2026-06-11 08:04:52 -0700

    save

--- architecturemod/src/main/java/net/minecraft/architecturemod/ArchitectureModClient.java
+++ architecturemod/src/main/java/net/minecraft/architecturemod/ArchitectureModClient.java
@@ -1,41 +1,16 @@
 package net.minecraft.architecturemod;
 
-import com.mojang.serialization.MapCodec;
-import com.sun.jna.platform.win32.WinGDI;
 import net.minecraft.architecturemod.block.ModBlocks;
-import net.minecraft.client.renderer.rendertype.RenderType;
-
-import java.awt.image.ColorModel;
 import java.util.List;
-import java.util.TreeSet;
-
 import net.minecraft.client.color.block.BlockTintSource;
-import net.minecraft.client.color.block.BlockTintSources;
-import net.minecraft.client.color.item.ItemTintSources;
 import net.minecraft.client.renderer.block.BlockAndTintGetter;
-import net.minecraft.client.renderer.block.FluidModel;
-import net.minecraft.client.resources.model.sprite.Material;
 import net.minecraft.core.BlockPos;
-import net.minecraft.resources.Identifier;
-import net.minecraft.util.ExtraCodecs;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.TintedGlassBlock;
-import net.minecraft.world.level.block.TransparentBlock;
 import net.minecraft.world.level.block.state.BlockState;
-
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.render.fluid.v1.FluidRenderingRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.BlockColorRegistry;
-import net.minecraft.client.color.item.ItemTintSource;
-import net.minecraft.architecturemod.ArchitectureMod;
-import org.w3c.dom.css.RGBColor;
-
-import javax.swing.text.AttributeSet;
-
-import static com.mojang.serialization.codecs.RecordCodecBuilder.instance;
-import static com.mojang.serialization.codecs.RecordCodecBuilder.mapCodec;
-import static org.apache.commons.lang3.function.Functions.apply;
-
+import net.fabricmc.fabric.api.client.rendering.v1.BlockRenderLayerMap;
+import net.minecraft.client.renderer.entity.layers.RenderLayer;
 
 public class ArchitectureModClient implements ClientModInitializer {
 
